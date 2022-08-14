d395591dc2337d9dc312824b7af6c5d205d0c610
Bisrat Girma <bisratx@gmail.com>
2022-08-14 16:57:13 +0300

    token back

--- lib/data/api/api_client.dart
+++ lib/data/api/api_client.dart
@@ -1,16 +1,17 @@
 import 'package:get/get.dart';
 
 class ApiClient extends GetConnect implements GetxService {
-  //late String token;
+  late String token;
   final String appBaseUrl;
   late Map<String, String> _mainHeaders;
 
   ApiClient({required this.appBaseUrl}) {
     baseUrl = appBaseUrl;
-    timeout = Duration(seconds: 30);
+    timeout = const Duration(seconds: 30);
+    token = '';
     _mainHeaders = {
       'Content-type': 'application/json; charset=UTF-8',
-      'Authorization': 'Bearer token',
+      'Authorization': 'Bearer $token',
     };
   }
   Future<Response> getData(String uri) async {
